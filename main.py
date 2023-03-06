c7173c6d9d2839681b0ed283058a370bf9249ec5
Sajid <95146062+SomeOrdinaryBro@users.noreply.github.com>
2023-03-06 20:57:50 +0530

    Added some little features
    added a way for the user to exit program whenever by tying exit
    error handing: wrong link, network errors and api limit exceed
    
    now the code creates a log of all the links youve searched and only create a report if the link is malicious

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import os
-import webbrowser
+import argparse
 import requests
 
-API_KEY = 'API_KEY_GOES_HERE' #You can get your api key from this link >> https://www.virustotal.com/
-
-def scan_url(url):
+def scan_url(api_key, url):
     url = url.strip()
-    params = {'apikey': API_KEY, 'resource': url}
+    params = {'apikey': api_key, 'resource': url}
     headers = {
         'Accept-Encoding': 'gzip, deflate',
         'User-Agent': 'gzip, My Python requests library example client or username'
     }
-    response = requests.get('https://www.virustotal.com/vtapi/v2/url/report', params=params, headers=headers)
+
+    try:
+        response = requests.get('https://www.virustotal.com/vtapi/v2/url/report', params=params, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f'An error occurred while scanning the URL: {e}')
+        return
+
     json_response = response.json()
 
     if json_response['response_code'] == 0:
@@ -20,34 +25,55 @@ def scan_url(url):
 
     if json_response['positives'] == 0:
         print('The URL is safe: ' + url)
-        return
+    else:
+        print('The URL is malicious: ' + url)
+        print('Positive detections: ' + str(json_response['positives']))
+        print('Total detections: ' + str(json_response['total']))
 
-    print('The URL is malicious: ' + url)
-    print('Positive detections: ' + str(json_response['positives']))
-    print('Total detections: ' + str(json_response['total']))
     url_report = json_response['permalink']
-    filename = url.replace('/', '_') + '_scan_report.html'
-    with open(filename, 'w') as f:
-        f.write(f'<html><body><a href="{url_report}">{url_report}</a></body></html>')
-    print('A detailed report has been downloaded in the same directory as this script: ' + os.path.abspath(filename))
-    webbrowser.open_new_tab(os.path.abspath(filename))
+    if json_response['positives'] > 0:
+        filename = url.replace('/', '_') + '_scan_report.html'
+        try:
+            with open(filename, 'w') as f:
+                f.write(f'<html><body><a href="{url_report}">{url_report}</a></body></html>')
+        except Exception as e:
+            print(f'An error occurred while writing to the file: {e}')
+            return
 
-def main():
+        print(f'A detailed report has been saved in the same directory as this script: {os.path.abspath(filename)}')
+    else:
+        print('No detailed report has been created as the URL is safe.')
+
+    # Append the URL and its scan report to a log file
+    log_filename = 'scan_log.txt'
+    with open(log_filename, 'a') as f:
+        f.write(f'URL: {url}\n')
+        if json_response['response_code'] != 0:
+            f.write(f'Positive detections: {json_response["positives"]}\n')
+            f.write(f'Total detections: {json_response["total"]}\n')
+            f.write(f'Scan report URL: {url_report}\n')
+        f.write('\n')
+
+def main(api_key):
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         print('Welcome to the VirusTotal URL scanner')
-        print('Please enter the URL you would like to scan:')
+        print('Please enter the URL you would like to scan (or "exit" to quit):')
         url = input()
+
+        if url.lower() == 'exit':
+            break
+
         try:
             response = requests.get(url)
             if response.status_code != 200:
                 print('The URL is not valid: ' + url)
                 continue
-        except:
+        except requests.exceptions.RequestException:
             print('The URL is not valid: ' + url)
             continue
 
-        scan_url(url)
+        scan_url(api_key, url)
 
         print('\nDo you want to scan another URL? y/n')
         answer = input()
@@ -55,4 +81,12 @@ def main():
             break
 
 if __name__ == '__main__':
-    main()
+    API_KEY = 'Your_API_Key_Goes_Here'
+    try:
+        response = requests.get('https://www.virustotal.com/vtapi/v2/url/report', params={'apikey': API_KEY, 'resource': 'https://www.google.com/'})
+        if response.status_code != 200:
+            print('Invalid API key or API limit exceeded')
+    except requests.exceptions.RequestException:
+        print('Invalid API key or API limit exceeded')
+    else:
+        main(api_key=API_KEY)
